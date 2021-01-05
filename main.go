0c339ae9d3442da5c939a06733a0850d700197eb
Dominic St-Pierre <dominicstpierre@gmail.com>
2021-01-05 17:10:36 -0500

    added cors on account init

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 
 	// account
 	acct := &accounts{}
-	http.HandleFunc("/account/init", acct.create)
+	http.Handle("/account/init", chain(http.HandlerFunc(acct.create), cors))
 	http.Handle("/account/auth", chain(http.HandlerFunc(acct.auth), requireRoot, withDB, cors))
 	http.Handle("/account/portal", chain(http.HandlerFunc(acct.portal), requireRoot, withDB, cors))
 
