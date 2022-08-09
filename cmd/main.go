a283badd3b4adeb61d03cf9d215013672a01abcb
Dominic St-Pierre <dominicstpierre@gmail.com>
2022-08-09 15:11:06 -0400

    added build info on compile (build time, commit and version)

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"os"
+
 	backend "github.com/staticbackendhq/core"
 	"github.com/staticbackendhq/core/config"
 )
 
 func main() {
 	c := config.LoadConfig()
+
+	var v bool
+	flag.BoolVar(&v, "v", false, "Display the version and build info")
+	flag.Parse()
+	if v {
+		fmt.Printf("StaticBackend version %s | %s (%s)\n\n",
+			config.Version,
+			config.BuildTime,
+			config.CommitHash,
+		)
+		os.Exit(0)
+	}
 
 	if len(c.Port) == 0 {
 		c.Port = "8099"
